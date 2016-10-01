5225cb43a9e753029523268d9f5800e750814a12
william-perley <n00636615@ospreys.unf.edu>
2016-10-01 14:15:11 -0400

    Gears

---
+++ Gears.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Gears {
+
+	public Scanner input;
+	public PrintStream ps;
+
+	public void run(){
+
+
+		class gear{
+			int x;
+			int y;
+			int radius;
+			boolean spinDir;
+
+		}
+	}
+	public gear meshes (){//takes in a gear
+
+
+	}
+
+	public static void main(String[] args) {
+		new Gears().run();
+	}
+}
